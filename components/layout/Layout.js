6df6e2c039be0e649292ebbf2ed01e6dbbc1a82a
Cole Miller <cole.t.miller@gmail.com>
2023-02-21 17:06:58 -0600

    updated main

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -3,9 +3,9 @@ import Navbar from "../sections/Navbar";
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="flex flex-col">
+      <div className="flex flex-col min-w-screen overflow-x-hidden bg-white text-black min-full-page">
         <Navbar />
-        <main>{children}</main>
+        <main className="flex-grow flex flex-col">{children}</main>
       </div>
     </>
   );
