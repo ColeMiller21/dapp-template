33a980e5bbef4ae8b681a3f5c862dee604e06512
Cole Miller <cole.t.miller@gmail.com>
2023-02-21 17:03:04 -0600

    updated main repo with rainbowkit

--- pages/_app.js
+++ pages/_app.js
@@ -1,61 +1,22 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import Layout from "../components/layout/Layout";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, WagmiConfig } from "wagmi";
+import { chains, wagmiClient } from "../helpers/walletConnect";
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
-import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 
-const fantomChain = {
-  id: 250,
-  name: "Fantom",
-  network: "fantom",
-  nativeCurrency: {
-    decimals: 18,
-    name: "Fantom",
-    symbol: "FTM",
-  },
-  rpcUrls: {
-    default: "https://rpc.ankr.com/fantom/",
-  },
-  blockExplorers: {
-    default: { name: "FTMScan", url: " https://ftmscan.com/" },
-  },
-  testnet: false,
-};
-
-const { chains, provider } = configureChains(
-  [chain.mainnet, fantomChain],
-  [
-    jsonRpcProvider({
-      priority: 0,
-      rpc: (chain) => {
-        if (chain.id !== fantomChain.id) return null;
-        return { http: chain.rpcUrls.default };
-      },
-    }),
-    alchemyProvider({ apiKey: "OSJnQ5JMZqgm8FdLA2s9-VgexHponnEX" }),
-    publicProvider(),
-  ]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: "Template dApp",
-  chains,
-});
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
-
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   const [showChild, setShowChild] = useState(false);
 
+  const appHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+  };
+
   useEffect(() => {
+    window.addEventListener("resize", appHeight);
+    appHeight();
     setShowChild(true);
   }, []);
 
@@ -79,5 +40,3 @@ function MyApp({ Component, pageProps }) {
     );
   }
 }
-
-export default MyApp;
